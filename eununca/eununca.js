810326308f4643051c013bf6c2bc7d3f31a79108
Tiago Lisboa <129691470+Lisboa06@users.noreply.github.com>
2023-12-09 23:48:54 -0300

    modificações gerais

--- eununca/eununca.js
+++ eununca/eununca.js
@@ -1,6 +1,5 @@
 const frase = [
 'participei de uma briga',
-'Furtei uma viatura',
 'chorei no transporte público',
 'fui expulso da sala de aula',
 'criei uma conta falsa nas redes sociais',
@@ -8,8 +7,20 @@ const frase = [
 'dei um tapa no rosto de alguém'
 ]
 
+const frasePesada = [
+'... Sem ideia por enquanto',
+'... Nada por aqui',
+'... Em breve novas opções por aqui'
+]
+
 function gerarFrase() {
     const randomElement = frase[Math.floor(Math.random() * frase.length)];
+    const fraseSaida = document.querySelector('#fraseId')
+    fraseSaida.innerHTML = randomElement
+}
+
+function gerarFrasePesada() {
+    const randomElement = frasePesada[Math.floor(Math.random() * frasePesada.length)];
     const fraseSaida = document.querySelector('#fraseId')
     fraseSaida.innerHTML = randomElement
 }
